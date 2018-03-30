ec29eb7b801684a320775742cf3f4dc05d7a4bdf
DengDa <dden5904@uni.sydney.edu.au>
2018-03-31 05:15:59 +1100

    finished version 1

---
+++ database/migrations/2018_03_30_181501_create_task_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user_id')->nullable();
+            $table->string('name')->nullable();
+            $table->string('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
