dfa602ae7601c3017e8a3ff2d049d079a697ef0b
DengDa <dden5904@uni.sydney.edu.au>
2018-03-31 04:09:11 +1100

    finished version 1

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport as ServiceProvider;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
